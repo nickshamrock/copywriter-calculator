bb556b6257fdd0ecbcfb97aa1573e5aaa7ad3c06
nickshamrock <nickshamrock1997@gmail.com>
2024-06-27 19:34:42 +0300

    Тестирование передачи данных между компонентами

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
-<script setup></script>
+<script setup>
+import TestComponentVue from './components/TestComponent.vue'
+
+const items = [
+  { name: 'Лев', surname: 'Shields', city: 'Ялуторовск' },
+  { name: 'Капитон', surname: 'Borer', city: 'Тотьма' },
+  { name: 'Вениамин', surname: 'Cummerata', city: 'Самара' },
+  { name: 'Прасковья', surname: 'Stokes', city: 'Владикавказ' },
+  { name: 'Анжелика', surname: 'Ullrich', city: 'Салават' }
+]
+</script>
 
 <template>
   <div class="mb-10 bg-red-500">Привет, мир!</div>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <TestComponentVue :items="items" />
 </template>
 
 <style></style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  items: Array
+})
+</script>
+
+<template>
+  <h2>Это дочерний компонент</h2>
+  <div v-for="item in items" :key="item.id">
+    {{ item.name }}
+  </div>
+</template>
