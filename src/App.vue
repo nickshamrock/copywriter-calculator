86ad4540e31fa48cfce101737b141b0d5ae30b24
nickshamrock <nickshamrock1997@gmail.com>
2024-07-04 17:55:53 +0300

    создание шаблона для проекта

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
-import { TestComp } from './components/TestComp.vue';
+import ChildComp from './components/ChildComp.vue';
+const items = [
+  { name: 'Лев', surname: 'Shields', city: 'Ялуторовск' },
+  { name: 'Капитон', surname: 'Borer', city: 'Тотьма' },
+  { name: 'Вениамин', surname: 'Cummerata', city: 'Самара' },
+  { name: 'Прасковья', surname: 'Stokes', city: 'Владикавказ' },
+  { name: 'Анжелика', surname: 'Ullrich', city: 'Салават' }
+];
 </script>
 
 <template>
   <div class="mb-10 bg-red-500">Привет, мир!</div>
-  <TestComp />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <ChildComp :items="items" />
 </template>
+
+<style></style>
---
+++ src/components/ChildComp.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  items: Array
+});
+</script>
+
+<template>
+  <h2>Это дочерний компонент</h2>
+  <div>Привет</div>
+  <div v-for="item in items" :key="item.id">
+    {{ item.name }}
+  </div>
+</template>
