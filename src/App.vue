baa6347dc877752a073c84d814f348f5886dad40
nickshamrock <nickshamrock1997@gmail.com>
2024-07-04 17:16:46 +0300

    тест настроек

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <script setup>
-import TestComponentVue from './components/TestComponent.vue'
-
-const items = [
-  { name: 'Лев', surname: 'Shields', city: 'Ялуторовск' },
-  { name: 'Капитон', surname: 'Borer', city: 'Тотьма' },
-  { name: 'Вениамин', surname: 'Cummerata', city: 'Самара' },
-  { name: 'Прасковья', surname: 'Stokes', city: 'Владикавказ' },
-  { name: 'Анжелика', surname: 'Ullrich', city: 'Салават' }
-]
+import { TestComp } from './components/TestComp.vue';
 </script>
 
 <template>
   <div class="mb-10 bg-red-500">Привет, мир!</div>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <TestComponentVue :items="items" />
+  <TestComp />
 </template>
-
-<style></style>
