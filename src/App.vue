3e3670827efb17278b13a27a954c614bca759830
nickshamrock <nickshamrock1997@gmail.com>
2024-07-19 18:46:35 +0300

    вывел в отдельный компонент меню подсчета стоимости текста

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,23 @@
 <script setup>
-import { computed, reactive, ref, watch } from 'vue';
-import gsap from 'gsap';
+import { ref } from 'vue';
 
 import ModalWindow from './components/ModalWindow.vue';
+import CalculatePriceMenu from './components/CalculatePriceMenu.vue';
 
 //логика подсчета стоимости текста
 const textFromTextarea = ref('');
 
-const countWithoutSpaces = computed(() => {
-  return textFromTextarea.value.replace(/\s+/gu, '').length;
-});
-
-const countWithSpaces = computed(() => {
-  return textFromTextarea.value.length;
-});
-
-const price = ref(100);
-
-const priceForText = ref('кликните на «Посчитать»');
-
-function getPriceForText() {
-  let result = (countWithoutSpaces.value / 1000) * price.value;
-  priceForText.value = result.toFixed(2) + ' руб.';
-
-  if (textFromTextarea.value === '') {
-    priceForText.value = 'Сначала введите текст!';
-  }
-}
-//функция очистки поля ввода текста
-function clearTextArea() {
+//получаем очистку поля ввода текста с помощью emit из дочернего компонента CalculatePriceMenu
+function clearArea() {
   textFromTextarea.value = '';
 }
 
 const showModal = ref(false);
-//анимация количества символов без пробелов в поле span
-const numberForWithoutSpaces = ref(countWithoutSpaces);
-const tweenedForWithoutSpaces = reactive({
-  number: 0
-});
-
-watch(numberForWithoutSpaces, (n) => {
-  gsap.to(tweenedForWithoutSpaces, { duration: 0.5, number: Number(n) || 0 });
-});
-
-const animatedCountWithoutSpaces = computed(() => {
-  return tweenedForWithoutSpaces.number.toFixed(0);
-});
-//анимация количества символов с пробелами в поле span
-const numberForWithSpaces = ref(countWithSpaces);
-const tweenedForWithSpaces = reactive({
-  number: 0
-});
-
-watch(numberForWithSpaces, (n) => {
-  gsap.to(tweenedForWithSpaces, { duration: 0.5, number: Number(n) || 0 });
-});
-
-const animatedCountWithSpaces = computed(() => {
-  return tweenedForWithSpaces.number.toFixed(0);
-});
 </script>
 
 <template>
   <div
-    class="m-auto flex h-full w-4/5 flex-col items-center rounded-xl bg-blue-300 pb-5 pl-5 pr-5 shadow-xl max-lg:w-full"
+    class="m-auto flex h-full w-4/5 flex-col items-center rounded-xl bg-blue-300 pb-10 pl-5 pr-5 shadow-xl max-lg:w-full"
     @keydown.esc="showModal = false"
   >
     <div class="flex flex-col items-center">
@@ -87,7 +41,7 @@ const animatedCountWithSpaces = computed(() => {
           @click="showModal = true"
           id="show-modal"
       /></label>
-
+      <!-- модальное поп-ап окно -->
       <ModalWindow :show="showModal" @close="showModal = false"> </ModalWindow>
 
       <textarea
@@ -97,59 +51,8 @@ const animatedCountWithSpaces = computed(() => {
         placeholder="Напишите или скопируйте сюда текст..."
         v-model="textFromTextarea"
       ></textarea>
-    </div>
-
-    <div class="mt-5 flex justify-between gap-[15px] max-sm:flex-col max-sm:gap-4">
-      <div class="flex flex-col gap-3">
-        <p class="text-lg">
-          Ставка:
-          <input
-            type="number"
-            min="0"
-            value="100"
-            autocomplete="off"
-            placeholder="Введите сумму"
-            class="ml-2 mr-2 w-16 rounded-xl p-1 text-lg"
-            id="input-for-price"
-            v-model="price"
-          />₽ за 1 000 символов без пробелов
-        </p>
-
-        <p class="text-lg">
-          Итоговая сумма:
-          <span class="inline-block rounded-xl bg-white p-2 text-lg">
-            {{ priceForText }}
-          </span>
-        </p>
-        <p class="text-lg">
-          Кол-во символов без пробелов:
-          <span class="inline-block rounded-xl bg-white p-2 text-lg">{{
-            animatedCountWithoutSpaces
-          }}</span>
-        </p>
-        <p class="text-lg">
-          Кол-во символов с пробелами:
-          <span class="inline-block rounded-xl bg-white p-2 text-lg">{{
-            animatedCountWithSpaces
-          }}</span>
-        </p>
-      </div>
-      <div class="flex flex-col justify-center gap-5">
-        <button
-          class="select-none rounded-full border border-gray-900 bg-white px-6 py-3 text-center align-middle text-base font-bold uppercase text-gray-900 transition-all hover:bg-green-500 focus:ring focus:ring-green-400 active:opacity-[0.85]"
-          type="button"
-          @click="getPriceForText"
-        >
-          Посчитать
-        </button>
-        <button
-          class="select-none rounded-full border border-gray-900 bg-white px-6 py-3 text-center align-middle text-base font-bold uppercase text-gray-900 transition-all hover:bg-red-400 focus:ring focus:ring-red-400 active:opacity-[0.85]"
-          type="reset"
-          @click="clearTextArea"
-        >
-          Очистить
-        </button>
-      </div>
+      <!-- меню с расчетом стоимости текста -->
+      <CalculatePriceMenu :text="textFromTextarea" @makeEmptyArea="clearArea"></CalculatePriceMenu>
     </div>
   </div>
 </template>
